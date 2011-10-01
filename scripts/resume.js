4f5b65ad34db604ea9895f336979299dc72a7a1a
D4LatCSC <aj5lee@caffeine.csclub.uwaterloo.ca>
2011-10-01 03:49:39 -0400

    Added alot of javascript and implemented the commentary system

---
+++ scripts/resume.js
@@ -0,0 +1,56 @@
+//SET the commentary text constants
+var resumeCommentary = {};
+$.getJSON('../data/resumeCommentary.json', function(data) {
+  $.each(data, function(key, val) {
+    resumeCommentary[key] = val;
+  });
+});
+
+//This is most of the commentary functions
+function resizeCommentary () {
+    commentaryWidth = $('body').width() - $('#bodyContainer').width() - 50;
+    $('#commentary').width(commentaryWidth);
+};
+function followScroll() {
+  $('#commentary').stop().animate({
+    marginTop: $(window).scrollTop() + 40
+  }, 200);
+  //$('#commentary').css('margin-top',$(window).scrollTop());
+};
+function changeCommentary(picker) {
+  $('#commentary').animate({
+    opacity: 0
+  }, 200, function(){
+    $(this).text(resumeCommentary[picker]);
+  }).animate({
+    opacity: 0.7
+  }, 200);
+};
+
+//This stuff will run when document is loaded
+$(document).ready(function () {
+  resizeCommentary();
+  $(window).resize(function() {
+    resizeCommentary();
+  });
+
+  $(window).scroll(function() {
+    followScroll();
+  });
+
+  $('.genericSection').mouseenter(function () {
+    $(this).addClass('selectedSection').animate({
+      "padding-left": 20
+    }, 200);
+  });
+
+  $('.genericSection').mouseleave(function() {
+    $(this).removeClass('selectedSection').animate({
+      "padding-left": 0
+    }, 200);
+  });
+
+  $('.commentaryChanger').mouseenter(function () {
+    changeCommentary($(this).attr('picker'));
+  });
+});
