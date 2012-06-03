c2b28bc8fd3c4e6548fe5a036aa76d74e5e4d303
Austin Lee D4L <Austin.L.D4L@gmail.com>
2012-06-02 22:45:49 -0400

    Create link_to method, empty still

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
   <head>
     <title>The Abyss</title>
     <script type="text/javascript" src="scripts/jquery-1.6.4.min.js"></script>
+    <?php
+      include 'php/link_to.php';
+    ?>
   </head>
   <body>
     <?php echo "Hello World!"?>
+    <?php link_to() ?>
     <?php echo "Let's start writing php functions!" ?>
   </body>
 </html>
---
+++ php/link_to.php
@@ -0,0 +1,7 @@
+<?php
+
+function link_to() {
+  echo "external";
+}
+
+?>
